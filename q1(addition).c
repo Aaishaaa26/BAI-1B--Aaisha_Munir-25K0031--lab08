5760be8abed4854964ba1fd6614dbc9c7b5b3a2e
Aaisha Munir <cakefunneh123@gmail.com>
2025-10-23 09:35:05 +0500

    Update q1(addition).c

--- q1(addition).c
+++ q1(addition).c
@@ -6,10 +6,10 @@ int main(){
 	printf("Enter display limit: ");
 	scanf("%d",&limit);
 		printf("Multiplication table \n");
-	for(i=1;i<num;i++){
+	for(i=1;i<=limit;i++){
 		printf("%d:",i);
-		for(j=1;j<limit;j++){
-			printf("%4d",i*j);
+		for(j=1;j<num;j++){
+			printf("\t%d",i*j);
 		}
 		printf("\n");
 	}
